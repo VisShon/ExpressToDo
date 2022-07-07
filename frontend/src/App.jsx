936f4c892a7f291dede23c7ddabd6d32b430bd2c
VisShon <vishnu20414@iiitd.ac.in>
2022-07-07 17:51:48 +0530

    basic frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+      <div className="Body">
+        <span className="heading">Express ToDo</span>
+        <div className="cards">
+          {}
+        </div>
+        <a href="#" className="button">Add Goal</a>
+      </div>
+  );
+}
+
+export default App;
