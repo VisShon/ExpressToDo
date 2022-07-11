bf64f1d891a9f27b173a032e5c32dde51acb7bb4
VisShon <vishnu20414@iiitd.ac.in>
2022-07-11 10:42:07 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,36 +6,54 @@ import bcg from './bcg.png';
 
 
 function App() {
+  
   const [data,setData] = useState(null)
+  const [newGoal,setNewGoal] = useState('')
   useEffect((() => {
     async function fetchData(){
       const newData = await axios.get('http://localhost:8000/api/goals').then(D=>setData(D.data));
     }
     fetchData();
   }),[]);
-
-  const onClick= async (Data)=>{
+  const onInput = async() =>{
+    setNewGoal(document.getElementById('input').value);
+    console.log(newGoal);
+  }
+  const onAddGoal= async ()=>{
     console.log('poop')
     const bg=document.getElementById("background");
     const pop=document.getElementById("popUp");
     const btn=document.getElementById("addGoal");
+    const btn2=document.getElementById("Done");
+    btn2.hidden=false;
     btn.hidden=true;
     pop.hidden=false;
     bg.hidden=false;
-    // await axios.post('https://jsonplaceholder.typicode.com/todos',{
-      
-    // })
+  }
+
+  const onDone = async () =>{
+    console.log('poop')
+    const bg=document.getElementById("background");
+    const pop=document.getElementById("popUp");
+    const btn=document.getElementById("addGoal");
+    const btn2=document.getElementById("Done");
+    btn2.hidden=true;
+    btn.hidden=false;
+    pop.hidden=true;
+    bg.hidden=true;
+    const newData = await axios.post(`http://localhost:8000/api/goals`,{title:newGoal});
   }
 
   return (
       <div className="Body">
         <span className="heading">Express To-Do</span>
         <Todo Data={data}/>
-        <a href="#" id='addGoal' className="button" onClick={onClick}>Add Goal</a>
-        <img id="background" src={bcg} hidden={true}></img>
-        <div id="popUp" hidden={true}>
-          <input type="text" placeholder='next goal?' className='textfield'></input>
-        </div>
+        <a href="#" id='addGoal' className="button" onClick={onAddGoal}>Add Goal</a>
+          <a href="#" id='Done' className="button" onClick={onDone} hidden={true}>Done</a>
+          <img id="background" src={bcg} hidden={true}></img>
+          <div id="popUp" hidden={true}>
+            <input id="input" type="text" placeholder='next goal?' className='textfield' onKeyDown={onInput}></input>
+          </div>
       </div>
   );
 }
