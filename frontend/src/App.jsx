667e73e8cf8bdf289366a048b9b6015f1d8831f1
VisShon <vishnu20414@iiitd.ac.in>
2022-07-11 18:18:55 +0530

    Post goal works only check handler remains

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,54 +6,34 @@ import bcg from './bcg.png';
 
 
 function App() {
-  
+  const [popUp,setIsPopUp] =useState(false);
   const [data,setData] = useState(null)
   const [newGoal,setNewGoal] = useState('')
   useEffect((() => {
     async function fetchData(){
       const newData = await axios.get('https://expressgoalsapi.herokuapp.com/api/goals').then(D=>setData(D.data));
+      console.log('poop')
     }
     fetchData();
-  }),[]);
-  const onInput = async() =>{
-    setNewGoal(document.getElementById('input').value);
-    console.log(newGoal);
-  }
-  const onAddGoal= async ()=>{
-    console.log('poop')
-    const bg=document.getElementById("background");
-    const pop=document.getElementById("popUp");
-    const btn=document.getElementById("addGoal");
-    const btn2=document.getElementById("Done");
-    btn2.hidden=false;
-    btn.hidden=true;
-    pop.hidden=false;
-    bg.hidden=false;
-  }
+  }),[popUp]);
 
   const onDone = async () =>{
-    console.log('poop')
-    const bg=document.getElementById("background");
-    const pop=document.getElementById("popUp");
-    const btn=document.getElementById("addGoal");
-    const btn2=document.getElementById("Done");
-    btn2.hidden=true;
-    btn.hidden=false;
-    pop.hidden=true;
-    bg.hidden=true;
-    const newData = await axios.post(`https://expressgoalsapi.herokuapp.com/api/goals`,{title:newGoal});
+    const postData = await axios.post(`https://expressgoalsapi.herokuapp.com/api/goals`,{title:newGoal});
+    setIsPopUp(false);
   }
 
   return (
       <div className="Body">
         <span className="heading">Express To-Do</span>
         <Todo Data={data}/>
-        <a href="#" id='addGoal' className="button" onClick={onAddGoal}>Add Goal</a>
-          <a href="#" id='Done' className="button" onClick={onDone} hidden={true}>Done</a>
-          <img id="background" src={bcg} hidden={true}></img>
-          <div id="popUp" hidden={true}>
-            <input id="input" type="text" placeholder='next goal?' className='textfield' onKeyDown={onInput}></input>
+        {!popUp && <a href="#" id='addGoal' className="button" onClick={e => setIsPopUp(true)}>Add Goal</a>}
+          {popUp && (<>
+          <a href="#" id='Done' className="button" onClick={onDone}>Done</a>
+          <img id="background" src={bcg}></img>
+          <div id="popUp">
+            <input id="input" type="text" placeholder='next goal?' className='textfield' value={newGoal} onChange={e => setNewGoal(e.target.value)}></input>
           </div>
+                  </>)}
       </div>
   );
 }
