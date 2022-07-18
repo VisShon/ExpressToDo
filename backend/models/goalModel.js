eea3cb2cac5edc7818906913501304c27ef764d7
VisShon <vishnu20414@iiitd.ac.in>
2022-07-18 17:02:58 +0530

    updates

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema({
     id:{
-        type: Integer,
+        type: Number,
         auto: true,
         require: true,
         index: true,
